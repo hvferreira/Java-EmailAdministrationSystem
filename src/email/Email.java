5582f61e1438c34c2dd2f394ef6340543cbda2c5
hvferreira <heldervferreira@gmail.com>
2023-12-07 18:27:32 +0000

    create set departement

--- src/email/Email.java
+++ src/email/Email.java
@@ -7,7 +7,7 @@ public class Email {
 
     private String firstName;
     private String lastName;
-    private String dept;
+    private String department;
     private String email;
     private String password;
     private String alternativeEmail;
@@ -21,5 +21,31 @@ public class Email {
         this.lastName = lastName;
         System.out.println("New Employee:"+this.firstName+""+this.lastName);
     }
+    private String generate_email() {
+        return this.firstName.toLowerCase() + "." + this.lastName.toLowerCase() + "@" + this.department.toLowerCase()
+                + ".company.com";
+    }
 
+    private String setDepartment() {
+        System.out.println(
+                "DEPARTMENT CODES\n1 for Sales\n2 for Development\n3 for Accounting\n0 for None");
+        boolean bool = false;
+        do {
+            System.out.print("Enter Department Code: ");
+            int choice = sc.nextInt();
+            switch (choice) {
+                case 1:
+                    return "Sales";
+                case 2:
+                    return "Development";
+                case 3:
+                    return "Accounting";
+                case 0:
+                    return "None";
+                default:
+                    System.out.println("**INVALID CHOICE**");
+            }
+        } while (!bool);
+        return null;
+    }
 }
