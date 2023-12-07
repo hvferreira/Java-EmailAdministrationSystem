fa57081a0c9c0834867b5e6d3bdef273ae3b1baf
hvferreira <heldervferreira@gmail.com>
2023-12-07 18:15:51 +0000

    create var and Constructor

--- src/email/Email.java
+++ src/email/Email.java
@@ -1,4 +1,25 @@
 package email;
 
+import java.util.Scanner;
+import java.util.stream.StreamSupport;
+
 public class Email {
+
+    private String firstName;
+    private String lastName;
+    private String dept;
+    private String email;
+    private String password;
+    private String alternativeEmail;
+    private int mailCapacity=500;
+
+
+    Scanner sc = new Scanner(System.in);
+
+    public Email(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        System.out.println("New Employee:"+this.firstName+""+this.lastName);
+    }
+
 }
